72a75dd5a29037260edc5e430d6c41f6eacccdcb
Venom <muhammet.karahan@lepoles.com>
2020-10-21 16:20:01 +0200

    Exercice-HTML

--- java.js
+++ java.js
@@ -1,28 +1,61 @@
-var plus = document.getElementById(plus); 
-var moins = document.getElementById(moins);
-var fois = document.getElementById(fois);
-var div = document.getElementById(div);
-var puissance = document.getElementById(puissance);
+// var plus = document.getElementById("plus"); 
+// var moins = document.getElementById("moins");
+// var fois = document.getElementById("fois");
+// var div = document.getElementById("div");
+// var puissance = document.getElementById("puissance");
 
 
 
-function onClickPlus(){
-    var result = document.querySelector("#resultat");
-    var chiffre1 = document.querySelector("#chiffre1").value;
-    var chiffre2 = document.querySelector("#chiffre2").value;
-    var result = Number(chiffre1) + Number(chiffre2);
-    result.value = resultat;
-}
+// function onClickPlus(){
+//     var result = document.querySelector("#resultat");
+//     var chiffre1 = document.querySelector("#chiffre1").value;
+//     var chiffre2 = document.querySelector("#chiffre2").value;
+//     var resultat = Number(chiffre1) + Number(chiffre2);
+//     result.value = resultat;
+// }
 
-function onClickMoins(){
-    var result = document.querySelector("#resultat");
-    var chiffre1 = document.querySelector("#chiffre1").value;
-    var chiffre2 = document.querySelector("#chiffre2").value;
-    var resultat = Number(chiffre1) - Number(chiffre2);
-    result.value = resultat;
-  }
 
-  
-plus.addEventListener("click", onClickPlus);
-moins.addEventListener("click", onClickMoins);
+// function onClickMoins(){
+//   var result = document.querySelector("#resultat");
+//   var chiffre1 = document.querySelector("#chiffre1").value;
+//   var chiffre2 = document.querySelector("#chiffre2").value;
+//   var resultat = Number(chiffre1) - Number(chiffre2);
+//   result.value = resultat;
+// }
 
+// function onClickFois(){
+//   var result = document.querySelector("#resultat");
+//   var chiffre1 = document.querySelector("#chiffre1").value;
+//   var chiffre2 = document.querySelector("#chiffre2").value;
+//   var resultat = Number(chiffre1) * Number(chiffre2);
+//   result.value = resultat;
+// }
+
+// function onClickDiv(){
+//   var result = document.querySelector("#resultat");
+//   var chiffre1 = document.querySelector("#chiffre1").value;
+//   var chiffre2 = document.querySelector("#chiffre2").value;
+//   var resultat = Number(chiffre1) / Number(chiffre2);
+//   result.value = resultat;
+// }
+
+// function onClickPuissance(){
+//   var result = document.querySelector("#resultat");
+//   var chiffre1 = document.querySelector("#chiffre1").value;
+//   var chiffre2 = document.querySelector("#chiffre2").value;
+//   var resultat = Number(chiffre1) ** Number(chiffre2);
+//   result.value = resultat;
+// }
+ 
+
+// plus.addEventListener("click", onClickPlus);
+// moins.addEventListener("click", onClickMoins);
+// fois.addEventListener("click", onClickFois);
+// div.addEventListener("click", onClickDiv);
+// puissance.addEventListener("click", onClickPuissance);
+
+
+
+// ----------------------------------
+
+alert("Hello ! ");
